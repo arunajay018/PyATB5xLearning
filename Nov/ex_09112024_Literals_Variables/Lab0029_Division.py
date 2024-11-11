cc8a82d936449143530c0c550d93de0935888b9f
arunajay018 <arunajaygopal@gmail.com>
2024-11-11 21:14:25 +0530

    Task given 09/11/24

---
+++ Nov/ex_09112024_Literals_Variables/Lab0029_Division.py
@@ -0,0 +1,6 @@
+num1=int(input("enter first value"))
+num2=int(input("enter second value"))
+Quotient = (num1//num2)
+Remainder = num1%num2
+print(Quotient)
+print(Remainder)
---
+++ Nov/ex_09112024_Literals_Variables/Lab0030_Arithmetic_operation_with3input.py
@@ -0,0 +1,13 @@
+num1=float(input("enter first number"))
+num2=float(input("enter second number"))
+num3=float(input("enter third number"))
+
+sum = num1 + num2 + num3
+difference = num1-num2-num3
+product = num1*num2*num3
+Quotient=num1//num2//num3
+Remainder= num1%num2%num3
+print(sum,difference,product,Quotient,Remainder)
+
+
+
